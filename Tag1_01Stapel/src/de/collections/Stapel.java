ae8d54a14a37d398a9714c5f2713e7560e807221
limagoHub <limagoHub@limago.de>
2020-02-27 10:20:13 +0100

    Initial Commit

---
+++ Tag1_01Stapel/src/de/collections/Stapel.java
@@ -0,0 +1,40 @@
+package de.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stapel {
+	
+	private static final int DEFAULT_SIZE = 10;
+	private List<Object> data;
+	
+	
+	public Stapel() {
+		data = new ArrayList<Object>(DEFAULT_SIZE);
+		
+	}
+	
+	public void push(Object value) throws StapelException{
+		if(isFull()) 
+			throw new StapelException("Overflow");
+		
+		data.add(value);
+	}
+	
+	public Object pop() throws StapelException {
+		if(isEmpty())
+			throw new StapelException("Underflow");
+		
+		return data.remove(data.size() - 1);
+	}
+	
+	public boolean isEmpty() {
+		return data.isEmpty();
+				
+	}
+	
+	public boolean isFull() {
+		return data.size() >= DEFAULT_SIZE;
+	}
+
+}
