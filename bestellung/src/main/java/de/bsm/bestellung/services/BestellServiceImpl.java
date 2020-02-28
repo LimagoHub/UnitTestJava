7f0c0b4395be5619d81c29aac42b801056f3bebc
limagoHub <limagoHub@limago.de>
2020-02-28 11:22:28 +0100

    Tests added

--- bestellung/src/main/java/de/bsm/bestellung/services/BestellServiceImpl.java
+++ bestellung/src/main/java/de/bsm/bestellung/services/BestellServiceImpl.java
@@ -1,13 +1,15 @@
 package de.bsm.bestellung.services;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import de.bsm.bestellung.repositories.BestellRepo;
 
 public class BestellServiceImpl implements BestellService {
 
 	private final CreditcardChecker creditcardChecker;
 	private final BestellRepo bestellRepo;
-	
-	
+	private final Pattern pattern = Pattern.compile("^(M|V)(\\d{10})$");
 	
 
 	public BestellServiceImpl(CreditcardChecker creditcardChecker, BestellRepo bestellRepo) {
@@ -15,16 +17,33 @@ public class BestellServiceImpl implements BestellService {
 		this.bestellRepo = bestellRepo;
 	}
 
-
-
-
 	@Override
 	public void bestellungErfassen(String bestellung, String kreditkartenNummer, double amount) throws BestellServiceException, KundePleiteException, ServerException {
-		if(bestellung == null)
-			throw new BestellServiceException("Bestellung darf nicht null sein.");
-		
-		if(kreditkartenNummer == null)
-			throw new BestellServiceException("Kreditkartennummer darf nicht null sein.");
+		try {
+			
+			if(bestellung == null)
+				throw new BestellServiceException("Bestellung darf nicht null sein.");
+			
+			if(kreditkartenNummer == null)
+				throw new BestellServiceException("Kreditkartennummer darf nicht null sein.");
+			
+			if(amount < 0.0) 
+				throw new BestellServiceException("Wert der Bestellung darf nicht negativ sein.");
+			
+			final Matcher matcher = pattern.matcher(kreditkartenNummer);
+			if(! matcher.matches()) throw new BestellServiceException("Kreditkartennummer hat ein falsches Format.");
+			
+			final String type = matcher.group(1).equals("M") ? "MASTER" : "VISA";
+			final String number = matcher.group(2);
+			
+			if (creditcardChecker.checkCreditcard(type, number, amount)) 
+				bestellRepo.save(bestellung);
+			else
+				throw new KundePleiteException();
+			
+		} catch (RuntimeException | CreditCardServerException e) {
+			throw new ServerException("Interner Serverfehler", e);
+		} 
 
 	}
 
--- bestellung/src/test/java/de/bsm/bestellung/services/BestellServiceImplTest.java
+++ bestellung/src/test/java/de/bsm/bestellung/services/BestellServiceImplTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
 
 import de.bsm.bestellung.repositories.BestellRepo;
 
@@ -46,5 +47,118 @@ public class BestellServiceImplTest {
 			assertEquals("Kreditkartennummer darf nicht null sein.", e.getMessage());
 		} 
 	}
+	
+	
+	@Test
+	public void bestellungErfassen_WrongCreditCardFormattooShort_throwsBestellServiceException() throws Exception{
+		try {
+			underTest.bestellungErfassen(VALID_ORDER, "V012345678", VALID_AMOUNT);
+			fail("SChwein gehabt");
+		} catch (BestellServiceException e) {
+			assertEquals("Kreditkartennummer hat ein falsches Format.", e.getMessage());
+		} 
+		
+	}
+	
+	
+	@Test
+	public void bestellungErfassen_WrongCreditCardFormattooLong_throwsBestellServiceException() throws Exception{
+		try {
+			underTest.bestellungErfassen(VALID_ORDER, "V01234567890", VALID_AMOUNT);
+			fail("SChwein gehabt");
+		} catch (BestellServiceException e) {
+			assertEquals("Kreditkartennummer hat ein falsches Format.", e.getMessage());
+		} 
+		
+	}
+	
+	@Test
+	public void bestellungErfassen_WrongCreditCardFormatNeitherVisaNorMaster_throwsBestellServiceException() throws Exception{
+		try {
+			underTest.bestellungErfassen(VALID_ORDER, "X0123456789", VALID_AMOUNT);
+			fail("SChwein gehabt");
+		} catch (BestellServiceException e) {
+			assertEquals("Kreditkartennummer hat ein falsches Format.", e.getMessage());
+		} 
+		
+	}
+	
+	
+	@Test
+	public void bestellungErfassen_AmountNegative_throwsBestellServiceException() throws Exception{
+		try {
+			underTest.bestellungErfassen(VALID_ORDER, VALID_MASTERCARD_NUMBER, -10.0);
+			fail("SChwein gehabt");
+		} catch (BestellServiceException e) {
+			assertEquals("Wert der Bestellung darf nicht negativ sein.", e.getMessage());
+		} 
+		
+	}
+	
+	@Test(expected = KundePleiteException.class)
+	public void bestellungErfassen_KundeInsolvent_throwsKundeIstPleiteException() throws Exception{
+		
+		when(creditcardCheckerMock.checkCreditcard(anyString(), anyString(), anyDouble())).thenReturn(false);
+		
+		underTest.bestellungErfassen(VALID_ORDER, VALID_MASTERCARD_NUMBER, VALID_AMOUNT);
+		
+		
+	}
+
+	@Test(expected = ServerException.class)
+	public void bestellungErfassen_CreditcardServerDown_throwsServerDownException() throws Exception{
+		
+		when(creditcardCheckerMock.checkCreditcard(anyString(), anyString(), anyDouble())).thenThrow(new CreditCardServerException());
+
+		underTest.bestellungErfassen(VALID_ORDER, VALID_MASTERCARD_NUMBER, VALID_AMOUNT);
+	}
+	
+	@Test
+	public void bestellungErfassen_CreditCardServerCall_CreditCardServerReceivesExpectedParameters() throws Exception{
+		
+		when(creditcardCheckerMock.checkCreditcard(anyString(), anyString(), anyDouble())).thenReturn(true);
+		underTest.bestellungErfassen(VALID_ORDER, VALID_MASTERCARD_NUMBER, VALID_AMOUNT);
+		verify(creditcardCheckerMock).checkCreditcard("MASTER", "0123456789", VALID_AMOUNT);
+	}
+	
+	
+	@Test(expected = ServerException.class)
+	public void bestellungErfassen_RepositoryDown_throwsServerDownException() throws Exception{
+		
+		when(creditcardCheckerMock.checkCreditcard(anyString(), anyString(), anyDouble())).thenReturn(true);
+		doThrow(new RuntimeException("Upps")).when(bestellRepoMock).save(anyString());
+		
+
+		underTest.bestellungErfassen(VALID_ORDER, VALID_MASTERCARD_NUMBER, VALID_AMOUNT);
+	}
+	
+	
+	
+	
+
+	@Test
+	public void bestellungErfassen_ProperUseMaster_success() throws Exception{
+		
+		when(creditcardCheckerMock.checkCreditcard(anyString(), anyString(), anyDouble())).thenReturn(true);
+		
+		underTest.bestellungErfassen(VALID_ORDER, VALID_MASTERCARD_NUMBER, VALID_AMOUNT);
+		
+		verify(bestellRepoMock).save(VALID_ORDER);
+	}
+	
+
+	@Test
+	public void bestellungErfassen_ProperUseVisa_success() throws Exception{
+		
+		when(creditcardCheckerMock.checkCreditcard(anyString(), anyString(), anyDouble())).thenReturn(true);
+		
+		underTest.bestellungErfassen(VALID_ORDER, VALID_VISACARD_NUMBER, VALID_AMOUNT);
+		
+		verify(bestellRepoMock).save(VALID_ORDER);
+	}
+	
+
+	
+	
 
 }
